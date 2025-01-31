4d4f0b86a38043ac1732629d0fb6b783c655793f
aliquader <alihackerreza123@gmail.com>
2025-01-31 21:50:15 +0330

    new files added

---
+++ morph-particles-master/src/Experience/sources.js
@@ -0,0 +1,47 @@
+export default [
+    // {
+    //     name: 'backgroundSound',
+    //     type: 'audio',
+    //     path: 'sounds/background.mp3'
+    // },
+    // {
+    //     name: 'predatorModel',
+    //     type: 'gltfModel',
+    //     path: 'models/predator.glb'
+    // },
+    // {
+    //     name: 'displacementTexture',
+    //     type: 'texture',
+    //     path: 'textures/displacement.jpg'
+    // }
+    {
+        name: 'e2Model',
+        type: 'gltfModel',
+        path: 'models/e2.glb'
+    },
+    // {
+    //     name: 'dnaModel',
+    //     type: 'gltfModel',
+    //     path: 'models/dna.glb'
+    // },
+    {
+        name: 'boyModel',
+        type: 'gltfModel',
+        path: 'models/untitledboy.glb'
+    },
+    {
+        name: 'oniModel',
+        type: 'gltfModel',
+        path: 'models/untitled.glb'
+    },
+    {
+        name: 'horseModel',
+        type: 'gltfModel',
+        path: 'models/horse.glb'
+    },
+    {
+        name: 'treeModel',
+        type: 'gltfModel',
+        path: 'models/tree.glb'
+    },
+]
